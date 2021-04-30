7dac2a81d149bd7ba908ab7a541b9a7ba5e567cf
teo3300 <matteo.rogora@live.it>
2021-04-30 07:20:47 +0200

    I ma be stupid

--- build/bmp16_func.d
+++
@@ -1,7 +0,0 @@
-bmp16_func.o: /home/rogora/progettiGBA/source/bmp16_func.c \
- /home/rogora/progettiGBA/include/bmp16_func.h \
- /home/rogora/progettiGBA/include/mem_type.h \
- /home/rogora/progettiGBA/include/memory_map.h
-/home/rogora/progettiGBA/include/bmp16_func.h:
-/home/rogora/progettiGBA/include/mem_type.h:
-/home/rogora/progettiGBA/include/memory_map.h:
--- build/sprite_test_4bpp.d
+++
@@ -1,4 +0,0 @@
-sprite_test_4bpp.o: \
- /home/rogora/progettiGBA/data_source/sprite_test_4bpp.c \
- /home/rogora/progettiGBA/data_header/sprite_test_4bpp.h
-/home/rogora/progettiGBA/data_header/sprite_test_4bpp.h:
